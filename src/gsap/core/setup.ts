d2f13457dd95e4c36aea6b902fcc4c476f788d70
Kensuke775 <t.ypebob96@gmail.com>
2026-04-11 16:01:42 +0900

    モバイル版gsap調整

--- src/gsap/core/setup.ts
+++ src/gsap/core/setup.ts
@@ -4,10 +4,12 @@ import { MotionPathPlugin } from "gsap/all";
 import { Draggable } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger);
-
 gsap.registerPlugin(MotionPathPlugin);
-
 gsap.registerPlugin(Draggable);
+
+// モバイルのアドレスバー/タブバーの出入りによる高さ変化で
+// ScrollTrigger が再計算されてピン位置がズレる問題を防ぐ
+ScrollTrigger.config({ ignoreMobileResize: true });
 
 
 /** OS / ブラウザの「動きを減らす」設定が有効か。 */
